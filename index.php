6ccb8e8d81e22f456d1442fd24b506d23362d543
Sebastien <sebblanc13@icloud.com>
2024-09-20 20:17:27 +0000

    #

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="FR_fr">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=11">
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulatelE11">
+    <meta http-equiv="X-UA-Compatible" content="IE=10">
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulatelE10">
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulatelE9">
+    <meta http-equiv="X-UA-Compatible" content="IE=8">
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulatelE8">
+    <meta http-equiv="X-UA-Compatible" content="IE=7">
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulatelE7">
+    <meta http-equiv="X-UA-Compatible" content="IE=5">
+
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Sébastien BLANC">
+    <meta property="og:title" content="Accueil - Sébastien BLANC">
+    <meta property="og:description" content="Bienvenue sur mon site internet ! Retrouvez mon CV en première page !">
+    <meta property="og:url" content="https://sebastien-blanc.fr">
+    <meta property="og:url_secure" content="https://sebastien-blanc.fr">
+    <meta property="og:image" content="https://sebastien-blanc.fr/public/content/assets/img/">
+    <meta property="og:image_secure" content="https://sebastien-blanc.fr/content/assets/img/">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:name" content="Accueil - Sebastien BLANC">
+    <meta name="twitter:description" content="Bienvenue sur mon site internet !">
+    <meta name="twitter:site" content="@SBlanc13000">
+    <meta name="twitter:creator" content="@SBlanc13000">
+
+    <link rel="apple-touch-icon" href="favicon.png">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+    <link rel="manifest" href="manifest.json">
+
+    <link rel="stylesheet" href="style.css">
+    <script src="./public/content/assets/js/main.js"></script>
+
+    <title>Accueil - Sébastien BLANC</title>
+</head>
+<body>
+    
+</body>
+</html>
